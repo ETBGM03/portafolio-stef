600a1f67a66ee68f62029495888cb055fba017b3
= <esfergo150618@gmail.com>
2021-10-17 19:27:42 -0500

    avance del portafolio responsive

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('#menu')
+let menuRes = document.querySelector('#menuResponsive')
+
+menuRes.addEventListener('click', () => {
+  menu.classList.toggle('nav__menu--togle')
+})
+
+

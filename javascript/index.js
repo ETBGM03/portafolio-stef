1b51af3db517b76025c268e55ef4397be0790159
= <esfergo150618@gmail.com>
2021-11-05 14:03:20 -0500

    Nuevas mejoras en diseño para movil, adelanto para tablets

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,13 @@
+// CODE FOR SET A NEW PROP IN CSS
 let menu = document.querySelector("#menu");
 let menuResponsive = document.querySelector("#menuResponsive");
-
+let ocultarDownloadCv = document.querySelector(".download-cv");
 menuResponsive.addEventListener("click", () => {
   menu.classList.toggle("nav__menu--togle");
+  ocultarDownloadCv.classList.toggle("ocultarBtn");
 });
 
+// CODE FOR SEND EMAIL A USER
 let form = document.querySelector("#form-contact");
 let mailto = document.querySelector("#trucazo");
 form.addEventListener("submit", enviarFormulario);
